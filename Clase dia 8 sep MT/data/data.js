9516d2a6aa4e9974993867599501fd9b1cc4b83a
Miguelzate1 <105441638+Miguelzate1@users.noreply.github.com>
2022-09-08 16:39:57 -0500

    Add files via upload

---
+++ Clase dia 8 sep MT/data/data.js
@@ -0,0 +1,43 @@
+export const data =
+[
+    {
+        "id":1,
+        "name":"Lago",
+        "image":"/img/Lago.jpg"
+    },
+    {
+
+        "id":2,
+        "name":"Montañas",
+        "image":"/img/Montañas.jpg"
+    },
+    {
+
+        "id":3,
+        "name":"Playa",
+        "image":"/img/Playa.jpg"
+    },
+    {
+        "id":4,
+        "name":"Cielo",
+        "image":"/img/Cielo.jpg"
+    },
+    {
+        "id":5,
+        "name":"Atardecer",
+        "image":"/img/Atardecer.jpg"
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+]
+
---
+++ Clase dia 8 sep MT/script/script.js
@@ -0,0 +1,25 @@
+import { data } from '../data/data.js';
+console.log(data)
+
+const items = document.getElementById('Items');
+const templateCard =document.getElementById('template-card').content;
+const fragment = document.createDocumentFragment();
+let like ={};
+
+document.addEventListener('DOMContentLoaded',()=>{
+    loadData(data)
+
+})
+
+const loadData = data =>{
+    data.forEach(personajes =>{
+        const {id,name,image}=personajes
+        templateCard.querySelector('h5').textContent = name;
+        templateCard.querySelector('img').setAttribute = ('src',image);
+        templateCard.querySelector('.btn-dark').dataset.id = id;
+const clone = templateCard.cloneNode(true)
+fragment.appendChild(clone)
+        
+    })
+    items.appendChild(fragment);
+}
